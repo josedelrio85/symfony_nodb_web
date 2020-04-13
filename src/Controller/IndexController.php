606ef08f405fe23bdc0b300232544f21eb520d2d
Jose del Río <josedelrio85@gmail.com>
2020-04-13 17:04:48 +0200

    eskeleton for product logic

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -6,47 +6,29 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+use App\Service\ProductService;
 
 class IndexController extends AbstractController {
 
-  public function __construct() {}
+  private $prodserv;
+
+  public function __construct(ProductService $prodserv) {
+    $this->prodserv = $prodserv;
+  }
   // TranslatorInterface $translator
   public function index(Request $request, TranslatorInterface $translator) {
-    // $slides = [];
-    // for ($i = 1; $i < 2; $i++) {
-    //   if ($translator->trans("{$provider}.home.slider.slide{$i}.background_image_name") ==
-    //                          "{$provider}.home.slider.slide{$i}.background_image_name"  ||
-    //       $translator->trans("{$provider}.home.slider.slide{$i}.background_image_name") == ""
-    //   ) {
-    //     continue;
-    //   }
 
-    //   array_push($slides,[
-    //     'background_image_name' => "{$provider}.home.slider.slide{$i}.background_image_name",
-    //     'background_image_name_mobile' => "{$provider}.home.slider.slide{$i}.background_image_name_mobile",
-    //     'title_text'            => "{$provider}.home.slider.slide{$i}.title_text",
-    //     'subtitle_text'         => "{$provider}.home.slider.slide{$i}.subtitle_text",
-    //     'megas_text'            => "{$provider}.home.slider.slide{$i}.megas_text",
-    //     'phone_text'            => "{$provider}.home.slider.slide{$i}.phone_text",
-    //     'tv_text'               => "{$provider}.home.slider.slide{$i}.tv_text",
-    //     'mobile_text'           => "{$provider}.home.slider.slide{$i}.mobile_text",
-    //     'land_line_text'        => "{$provider}.home.slider.slide{$i}.land_line_text",
-    //     'price_integer_text'    => "{$provider}.home.slider.slide{$i}.price_integer_text",
-    //     'price_decimals_text'   => "{$provider}.home.slider.slide{$i}.price_decimals_text",
-    //     'payment_period_text'   => "{$provider}.home.slider.slide{$i}.payment_period_text",
-    //     'promo_duration'        => "{$provider}.home.slider.slide{$i}.promo_duration",
-    //     'cta_icon'              => "{$provider}.home.slider.slide{$i}.cta_icon",
-    //     'cta_text'              => "{$provider}.home.slider.slide{$i}.cta_text",
-    //     'cta_url'               => "{$provider}.home.slider.slide{$i}.cta_url",
-    //     'cta_info_text'         => "{$provider}.home.slider.slide{$i}.cta_info_text",
-    //     'cta_info_url'          => "{$provider}.home.slider.slide{$i}.cta_info_url",
-    //     'nav_item_label'        => "{$provider}.home.slider.slide{$i}.nav_item_label",
-    //   ]);
-    // }
-    
-    // $translator = $translator->trans("header");
+    $products = $this->prodserv->getProducts();
+    $mainelements = $this->prodserv->getMain();
+    dump($mainelements);
+    $firstchildrens = $this->prodserv->getFirstChildren();
+    dump($firstchildrens);
 
-    
+    $secondChildrens = $this->prodserv->getSecondChildren();
+    dump($secondChildrens);
+    // die();
     $route = $request->attributes->get('_route');
     $uri = $request->server->get('REQUEST_URI');
 
@@ -54,6 +36,22 @@ class IndexController extends AbstractController {
       'route'     => $route,
       'uri'       => $uri,
       'translator' => $translator,
+      'mainelements' => $mainelements,
+      'firstchildrens' => $firstchildrens,
+      'secondChildrens' => $secondChildrens,
     ]);
+  }
+
+  public function getFirstChildren(Request $request) {
+    $element = $request->request->get('element');
+    $fc = $this->prodserv->getFirstChildren($element);
+
+    $response = array( 
+      "code" => 200,
+      "response" => $this->render('components/product-selector/product-card.html.twig', [
+        'fc' => $fc
+      ])->getContent() 
+    );
+    return new JsonResponse($response);
   }
 }
--- src/Service/ProductService.php
+++ src/Service/ProductService.php
@@ -4,112 +4,63 @@ namespace App\Service;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Yaml\Yaml;
 use App\Loader\YamlProductLoader;
-use App\Service\ProviderService;
 
 class ProductService {
   protected $productConfig;
-  protected $provider;
   protected $requestStack;
 
-  public function __construct(RequestStack $requestStack, string $rootPath) {   
+  public function __construct(RequestStack $requestStack, string $rootPath) {
     $configDir = $rootPath .DIRECTORY_SEPARATOR.'config';
     $this->productConfig = Yaml::parseFile($configDir.DIRECTORY_SEPARATOR.'product_config.yml');
-    $this->provider = (new ProviderService($requestStack))->getProvider();
   }
 
   public function getProducts() {
-    $info = [];
-    $names = [];
-    foreach($this->productConfig[$this->provider] as $name => $data) {
-      if($data['active']){
-        $names[$data['position']] = $name;
-        $info[$data['position']] = $data;
-      }
+    $children = [];
+    foreach($this->productConfig['products'] as $i => $data){
+      $children[$i] = $data['name'];
     }
-    ksort($names);
-    ksort($info);
+    return $children;
+  }
 
-    foreach($info as $k => $data){
-      $output[$names[$k]] = $data;
+  public function getMain() {
+    $elements = $this->productConfig['products'];
+    $output = [];
+    foreach($elements as $el) {
+      $output[] = $el['name'];
     }
     return $output;
   }
 
-  public function getConfigProduct($packID) {
-    $products = $this->getProducts();
-    if(array_key_exists($packID, $products)){
-      return $products[$packID];
-    }
-    return null;
-  }
-
-  public function getProductsHomepage() {
-    $output = null;
-    $products = $this->getProducts();
-    foreach($products as $name => $data){
-      if($data['homepage']){
-        $output[$data['position']] = $name;
-      }
-    }
-    ksort($output);
-    return $output;
-  }
-
-  public function getProductsFiberMobile() {
-    $output = null;
-    $products = $this->getProducts();
-    foreach($products as $name => $data){
-      $output[$data['position']] = $name;
-    }
-    ksort($output);
-    return $output;
-  }
-
-  public function getDataProductsFiberMobile() {
-    $output = null;
-    $products = $this->getProducts();
-    foreach($products as $name => $data){
-      $output[$data['position']]['path'] = $name;
-      $output[$data['position']]['values'] = $data;
-    }
-    ksort($output);
-    return $output;
-  }
-
-  public function getRelatedPacks($packID) {
-    $output = null;
-    $products = $this->getConfigProduct($packID);
-
-    if(!is_null($products)) {
-      foreach($products['related_packs'] as $pack => $values){
-        $output[] = $values;
-      }
+  public function getFirstChildren(){
+    $elements = $this->productConfig['products'];
+    $output = [];
+      foreach($elements as $el) {
+        foreach($el['children'] as $ch) {
+          // $output[$el['name']][] = $ch['name'];
+          $output[$el['name']][$ch['name']]['name'] = $ch['name'];
+          if(array_key_exists('path', $ch)){
+            $output[$el['name']][$ch['name']]['path'] = $ch['path'];
+          }
+        }
     }
     return $output;
   }
 
+  public function getSecondChildren(){
+    $elements = $this->productConfig['products'];
+    $output = [];
+      foreach($elements as $el) {
+        foreach($el['children'] as $ch) {
+          if(!is_null($ch['children'])){
+            foreach($ch['children'] as $r){
+              // $output[$ch['name']][] = $r['name'];
+              $output[$ch['name']][$r['name']]['name'] = $r['name'];
+              $output[$ch['name']][$r['name']]['path'] = $r['path'];
 
-  public function getRelatedInfo($packID) {
-    $output = null;
-    $products = $this->getConfigProduct($packID);
-
-    if(!is_null($products)) {
-      foreach($products['related_info'] as $pack => $values){
-        $output[] = $values;
-      }
+            }
+          }
+        }
     }
-    return $output;
-  }
-
-  public function getProductsSpecialPortability() {
-    $output = null;
-    $products = $this->getProducts();
-    foreach($products as $name => $data){
-      if($data['active'] && $data['fijo'] === 1 && $data['movil'] === 1){
-        $output[$data['position']] = $name;
-      }
-    }
-    ksort($output);
     return $output;
   }
 }
