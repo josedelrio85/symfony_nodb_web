b71eae5febbad5ed27d1588bce3a999f7a2ad976
Oscar Seoane <oscarseoane@bysidecar.onmicrosoft.com>
2020-04-15 13:45:46 +0200

    Fullscreen configurator

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   let closeButton = document.querySelector('.close-button');
   let fullScreenConfig = document.querySelector('.fullscreen-product-config');
 
-  document.querySelector('*[id^="product-"]').addEventListener('click', (event) => {
+  document.querySelector('.product-button').addEventListener('click', (event) => {
     fullScreenConfig.classList.add('active');
   });
 
@@ -16,127 +16,110 @@ document.addEventListener("DOMContentLoaded", function(event) {
     fullScreenConfig.classList.remove('active');
   });
 
+  let mainelements = document.querySelector('.fullscreen-product-config #mainelements');
+  let firstchildrens = document.querySelector('.fullscreen-product-config #firstchildrens');
+  let secondchildrens = document.getElementById('secondchildrens');
 
+  let productdivs = document.querySelectorAll('.product-button');
+  productdivs.forEach((cv, ci, listObj) => {
+    cv.addEventListener('click', (event) => {
+      // be careful when more elements are added to the card, maybe the id is not caputred properly
+      let id = event.target.parentNode.id;
 
-//   // Swiper test
-//   var mySwiper = new Swiper('.swiper-container', {
-//     speed: 400,
-//     spaceBetween: 100,
-//     pagination: {
-//         el: '.swiper-pagination',
-//         type: 'bullets',
-//         clickable: true
-//     },
-//     navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev',
-//     },
-// });
+      // hide mainelements div
+      mainelements.classList.add('d-none');
 
-//   let mainelements = document.getElementById('mainelements');
-//   let firstchildrens = document.getElementById('firstchildrens');
-//   let secondchildrens = document.getElementById('secondchildrens');
+      // show first childrens div
+      firstchildrens.classList.remove('d-none');
 
-//   let productdivs = document.querySelectorAll('*[id^="product-"]');
-//   productdivs.forEach((cv, ci, listObj) => {
-//     cv.addEventListener('click', (event) => {
-//       // be careful when more elements are added to the card, maybe the id is not caputred properly
-//       let id = event.target.parentNode.id;
+      // get id suffix to know what first child must show
+      let childrenid = id.replace('product-','');
+      document.querySelector('.fullscreen-product-config ' + '#fc-' + childrenid).classList.remove('d-none');
+    });
+  })
 
-//       // hide mainelements div
-//       mainelements.classList.add('d-none');
+  let fcdivs = document.querySelectorAll('.fullscreen-product-config *[id^="fc-"]');
+  fcdivs.forEach((cv, ci, listObj) => {
+    cv.addEventListener('click', (event) => {
+      // be careful when more elements are added to the card, maybe the id is not caputred properly
 
-//       // show first childrens div
-//       firstchildrens.classList.remove('d-none');
+      // get id suffix to know what second child must show
+      let idfc = event.target.parentNode.id;
+      let secondchildrenid = idfc.replace('sc-','');
 
-//       // get id suffix to know what first child must show
-//       let childrenid = id.replace('product-','');
-//       document.getElementById('fc-' + childrenid).classList.remove('d-none');
-//     });
-//   })
+      let secondchildrenelement = document.getElementById('sc-' + secondchildrenid);
+      if(secondchildrenelement != null){
 
-//   let fcdivs = document.querySelectorAll('*[id^="fc-"]');
-//   fcdivs.forEach((cv, ci, listObj) => {
-//     cv.addEventListener('click', (event) => {
-//       // be careful when more elements are added to the card, maybe the id is not caputred properly
+        // hide firstchildrens div
+        firstchildrens.classList.add('d-none');
 
-//       // get id suffix to know what second child must show
-//       let idfc = event.target.parentNode.id;
-//       let secondchildrenid = idfc.replace('sc-','');
+        // show secondchildrens div
+        secondchildrens.classList.remove('d-none');
 
-//       let secondchildrenelement = document.getElementById('sc-' + secondchildrenid);
-//       if(secondchildrenelement != null){
+        // show elements of secondchildren div too
+        secondchildrenelement.classList.remove('d-none');
+      }
+    });
+  })
 
-//         // hide firstchildrens div
-//         firstchildrens.classList.add('d-none');
+  // let back = document.querySelectorAll('*[id^="back-"]');
+  // back.forEach((cv, ci, listObj) => {
+  //   cv.addEventListener('click', (event) => {
 
-//         // show secondchildrens div
-//         secondchildrens.classList.remove('d-none');
+  //     let parentNode = event.target.parentNode;
+  //     if(parentNode.id === "firstchildrens"){
+  //       // if parentNode is firstchildrens, show mainelements
+  //       mainelements.classList.remove('d-none');
+  //     } else if (parentNode.id === "secondchildrens") {
+  //       // if parentNode is secondchildrens, show firstchildrens
+  //       firstchildrens.classList.remove('d-none');
+  //     }
 
-//         // show elements of secondchildren div too
-//         secondchildrenelement.classList.remove('d-none');
-//       }
-//     });
-//   })
+  //     let test = parentNode.id;
+  //     document.getElementById(test).classList.add('d-none');
 
-//   let back = document.querySelectorAll('*[id^="back-"]');
-//   back.forEach((cv, ci, listObj) => {
-//     cv.addEventListener('click', (event) => {
+  //     // hide all elementos of actual node except back-X
+  //     for (let item of parentNode.children) {
+  //       if(!item.id.includes('back-')){
+  //         item.classList.add('d-none');
+  //       }
+  //     }
+  //   });
+  // })
 
-//       let parentNode = event.target.parentNode;
-//       if(parentNode.id === "firstchildrens"){
-//         // if parentNode is firstchildrens, show mainelements
-//         mainelements.classList.remove('d-none');
-//       } else if (parentNode.id === "secondchildrens") {
-//         // if parentNode is secondchildrens, show firstchildrens
-//         firstchildrens.classList.remove('d-none');
-//       }
-
-//       let test = parentNode.id;
-//       document.getElementById(test).classList.add('d-none');
-
-//       // hide all elementos of actual node except back-X
-//       for (let item of parentNode.children) {
-//         if(!item.id.includes('back-')){
-//           item.classList.add('d-none');
-//         }
-//       }
-//     });
-//   })
-
-//   let launch = document.querySelectorAll('.launch');
-//   launch.forEach((cv, ci, listObj) => {
-//     cv.addEventListener('click', (event) => {
-//       // let id = event.target.id;
-//       // console.log(event);
+  // let launch = document.querySelectorAll('.launch');
+  // launch.forEach((cv, ci, listObj) => {
+  //   cv.addEventListener('click', (event) => {
+  //     // let id = event.target.id;
+  //     // console.log(event);
       
-//       // TODO decide what to do when a element with path is clicked. href?
-//     });
-//   });
+  //     // TODO decide what to do when a element with path is clicked. href?
+  //   });
+  // });
 
-//   function getData(id) {
-//     const urlEndPoint = '/first-children';
-//     console.log(id);
+  // function getData(id) {
+  //   const urlEndPoint = '/first-children';
+  //   console.log(id);
 
-//     let params = {
-//       element: id.replace('product-',''),
-//     }
+  //   let params = {
+  //     element: id.replace('product-',''),
+  //   }
 
-//     return new Promise((resolve, reject) => {
-//       landingCommander.makePostRequestFormData(params, urlEndPoint)
-//       .then((result) => {
-//         resolve(result);
-//       })
-//       .catch((error) => {reject(error);})
-//     });
-//   }
+  //   return new Promise((resolve, reject) => {
+  //     landingCommander.makePostRequestFormData(params, urlEndPoint)
+  //     .then((result) => {
+  //       resolve(result);
+  //     })
+  //     .catch((error) => {reject(error);})
+  //   });
+  // }
 
-//   // getData(id)
-//   //   .then((result) => {
-//   //     console.log(result.response);
-//   //     let mainproducts = document.getElementById('mainproducts');
-//   //     mainproducts.classList.add('d-none');
-//   //     document.getElementById('level2').innerHTML = result.response;
-//   //   })
-//   //   .catch((error) => { console.log(error); });
+  // // getData(id)
+  // //   .then((result) => {
+  // //     console.log(result.response);
+  // //     let mainproducts = document.getElementById('mainproducts');
+  // //     mainproducts.classList.add('d-none');
+  // //     document.getElementById('level2').innerHTML = result.response;
+  // //   })
+  // //   .catch((error) => { console.log(error); });
 });
