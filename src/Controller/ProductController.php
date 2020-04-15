3d8722afa374c16e6c14a3b19deb3efec6f6bcb8
Ivan Perez Fuentes <ivanprzf@gmail.com>
2020-04-15 17:11:11 +0200

    Added multiple components.
    This commit adds multiple components in order to create product landing page.
    There are some example data inside components to create loops in an easier way.

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -32,7 +32,7 @@ class ProductController extends AbstractController {
     // $route = $request->attributes->get('_route');
     // $uri = $request->server->get('REQUEST_URI');
 
-    return $this->render("pages/product.html.twig", [
+    return $this->render("pages/landing-product.html.twig", [
       // 'route'     => $route,
       // 'uri'       => $uri,
       // 'translator' => $translator,
@@ -42,41 +42,41 @@ class ProductController extends AbstractController {
     ]);
   }
 
-  public function getFirstChildren(Request $request) {
-    $element = $request->request->get('element');
-    $fc = $this->prodserv->getFirstChildren($element);
+  // public function getFirstChildren(Request $request) {
+  //   $element = $request->request->get('element');
+  //   $fc = $this->prodserv->getFirstChildren($element);
 
-    $response = array( 
-      "code" => 200,
-      "response" => $this->render('components/product-selector/product-card.html.twig', [
-        'fc' => $fc
-      ])->getContent() 
-    );
-    return new JsonResponse($response);
-  }
+  //   $response = array( 
+  //     "code" => 200,
+  //     "response" => $this->render('components/product-selector/product-card.html.twig', [
+  //       'fc' => $fc
+  //     ])->getContent() 
+  //   );
+  //   return new JsonResponse($response);
+  // }
 
-  public function test() {
-    $products = $this->prodserv->getProducts();
-    $mainelements = $this->prodserv->getMain();
-    dump($mainelements);
-    $firstchildrens = $this->prodserv->getFirstChildren();
-    dump($firstchildrens);
+  // public function test() {
+  //   $products = $this->prodserv->getProducts();
+  //   $mainelements = $this->prodserv->getMain();
+  //   dump($mainelements);
+  //   $firstchildrens = $this->prodserv->getFirstChildren();
+  //   dump($firstchildrens);
 
-    $secondChildrens = $this->prodserv->getSecondChildren();
-    dump($secondChildrens);
-    // die();
-    $route = $request->attributes->get('_route');
-    $uri = $request->server->get('REQUEST_URI');
+  //   $secondChildrens = $this->prodserv->getSecondChildren();
+  //   dump($secondChildrens);
+  //   // die();
+  //   $route = $request->attributes->get('_route');
+  //   $uri = $request->server->get('REQUEST_URI');
 
-    return $this->render("pages/index.html.twig", [
-      'route'     => $route,
-      'uri'       => $uri,
-      'translator' => $translator,
-      'mainelements' => $mainelements,
-      'firstchildrens' => $firstchildrens,
-      'secondChildrens' => $secondChildrens,
-    ]);
+  //   return $this->render("pages/index.html.twig", [
+  //     'route'     => $route,
+  //     'uri'       => $uri,
+  //     'translator' => $translator,
+  //     'mainelements' => $mainelements,
+  //     'firstchildrens' => $firstchildrens,
+  //     'secondChildrens' => $secondChildrens,
+  //   ]);
 
-  }
+  // }
 
 }
