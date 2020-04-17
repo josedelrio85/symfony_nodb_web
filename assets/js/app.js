2262e26382d03ebd99c42f9fa90b7995fdaac984
Jose del Río <josedelrio85@gmail.com>
2020-04-17 14:44:11 +0200

    fixed merge problems

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,27 +2,6 @@ import '../css/app.css';
 import 'bootstrap';
 import Swiper from 'swiper';
 import { bysidecar } from './bysidecar';
-
-document.addEventListener("DOMContentLoaded", function(event) {
-
-  ////////////////////// C2C ////////////////////////////////////////////
-  let C2cDeskop = document.querySelector('.click-to-call-desktop');
-  let C2cMobile = document.querySelector('.click-to-call-mobile');
-  let closeC2cDeskop = document.querySelector('.click-to-call-desktop .close-c2c');
-  let closeC2cMbile = document.querySelector('.click-to-call-mobile .close-c2c');
-  let C2cSide = document.querySelector('.click-to-call-btn');
-  let C2cMobileHeader = document.querySelector('.click-to-call-mobile .click-to-call--header');
-
-  closeC2cDeskop.addEventListener('click', (event) => {
-    C2cDeskop.classList.add('c2c-collapsed');
-    C2cSide.classList.add('c2c-size-open');
-  });
-
-  C2cSide.addEventListener('click', (event) => {
-    C2cDeskop.classList.remove('c2c-collapsed');
-    C2cSide.classList.remove('c2c-size-open');
-  });
-
 import { TweenMax, TimelineMax } from '../../node_modules/gsap/src/all.js';
 // import * as ScrollMagic from 'ScrollMagic';
 // import '../../node_modules/scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js';
@@ -30,22 +9,20 @@ import ScrollMagic from '../../node_modules/scrollmagic/scrollmagic/uncompressed
 import { ScrollMagicPluginGsap } from "scrollmagic-plugin-gsap";
 ScrollMagicPluginGsap(ScrollMagic, TweenMax, TimelineMax);
 
-import { landingCommander } from '../../node_modules/@bysidecar/landing_commander/dist/main';
-
 document.addEventListener("DOMContentLoaded", function(event) {
 
   // Slider home
   var sliderHome = new Swiper('.swiper-home', {
-      speed: 400,
-      pagination: {
-          el: '.swiper-pagination',
-          type: 'bullets',
-          clickable: true
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
+    speed: 400,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
   });
   
   // Fullscreen menu (decomment when we turn on menu links on header menu)
@@ -82,158 +59,23 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
   }
 
+  ////////////////////// C2C ////////////////////////////////////////////
+  let C2cDeskop = document.querySelector('.click-to-call-desktop');
+  let C2cMobile = document.querySelector('.click-to-call-mobile');
+  let closeC2cDeskop = document.querySelector('.click-to-call-desktop .close-c2c');
+  let closeC2cMbile = document.querySelector('.click-to-call-mobile .close-c2c');
+  let C2cSide = document.querySelector('.click-to-call-btn');
+  let C2cMobileHeader = document.querySelector('.click-to-call-mobile .click-to-call--header');
 
-  let closeButton = document.querySelector('.close-button');
-  let fullScreenConfig = document.querySelector('.fullscreen-product-config');
-
-  // let mainelements = document.querySelector('#mainelements');
-  let firstchildrens = document.querySelector('#firstchildrens');
-  let secondchildrens = document.getElementById('secondchildrens');
-  let backvalue = null;
-  let totalsteps = null;
-  let actualsteps = 1;
-
-  let productdivs = document.querySelectorAll('*[id^="product-"]');
-  productdivs.forEach((cv, ci, listObj) => {
-    cv.addEventListener('click', (event) => {
-
-      // be careful when more elements are added to the card, maybe the id is not caputred properly
-      let id = null;
-      if(event.target.nodeName === 'H5' || event.target.nodeName === "SPAN") {
-        id = event.target.parentNode.parentNode.id;
-      } else if(event.target.classList.contains('inner')){
-        id = event.target.parentNode.id;
-      } else {
-        id = event.target.id;
-      }
-      console.log(id);
-      // if element has href attribute let's navigate
-      let element = document.getElementById(id);
-      if (element.getAttribute('href') !== null) {
-        reset();
-        window.location.href = element.getAttribute('href');
-      } else {
-
-        fullScreenConfig.classList.add('active');
-
-        // show first childrens div
-        firstchildrens.classList.remove('d-none');
-  
-        // get id suffix to know what first child must show
-        // childrenid = salud | dental | mascotas | decesos
-        let childrenid = id.replace('product-','');
-  
-        // initialize steps bar and get totalsteps count
-        totalsteps = document.getElementById('steps-' + childrenid).value;
-        fillProgressBar(totalsteps);
-  
-        document.getElementById('fc-' + childrenid).classList.remove('d-none');
-      }
-    });
-  })
-
-  let fcdivs = document.querySelectorAll('*[id^="fc-"]');
-  fcdivs.forEach((cv, ci, listObj) => {
-    cv.addEventListener('click', (event) => {
-      // get id of element clicked
-      let idfc = null;
-      if(event.target.nodeName === "H5") {
-        idfc = event.target.parentNode.parentNode.id;
-      } else if(event.target.classList.contains("justify-content-between")){
-        idfc = event.target.parentNode.id;
-      } else {
-        idfc = event.target.id;
-      }
-      
-      // if element has href attribute let's navigate
-      let element = document.getElementById(idfc);
-      if (element.getAttribute('href') !== null) {
-        updateSteps(true);
-
-        setTimeout((out) => {
-          // close telon
-          fullScreenConfig.classList.remove('active');
-        }, 500);
-
-        setTimeout((out) => {
-          reset();
-          window.location.href = element.getAttribute('href');
-        }, 1000);
-      }
-
-      let secondchildrenelement = document.getElementById('sc-' + idfc);
-      if(secondchildrenelement != null){
-        // hide firstchildrens div
-        firstchildrens.classList.add('d-none');
-        // show secondchildrens div
-        secondchildrens.classList.remove('d-none');
-        // show elements of secondchildren div too
-        secondchildrenelement.classList.remove('d-none');
-        
-        updateSteps(true);
-
-        backvalue = 'sc-' + idfc;
-      }
-    });
-  })
-
-  let scdivs = document.querySelectorAll('*[id^="sc-"]');
-  scdivs.forEach((cv, ci, listObj) => {
-    cv.childNodes.forEach((e, f, g) => {
-      e.addEventListener('click', (event) => {
-
-        updateSteps(true);
-
-        // get id of element clicked
-        let idsc = null;
-        if(event.target.nodeName === "H5") {
-          idsc = event.target.parentNode.parentNode.id;
-        } else if(event.target.classList.contains("justify-content-between")){
-          idsc = event.target.parentNode.id;
-        } else {
-          idsc = event.target.id;
-        }
-        
-        // if element has href attribute let's navigate
-        let element = document.getElementById(idsc);
-        setTimeout((out) => {
-          // close telon
-          fullScreenConfig.classList.remove('active');
-        }, 500);
-
-        setTimeout((out) => {
-          reset();
-          window.location.href = element.getAttribute('href');
-        }, 1000);
-      });
-    });
+  closeC2cDeskop.addEventListener('click', (event) => {
+    C2cDeskop.classList.add('c2c-collapsed');
+    C2cSide.classList.add('c2c-size-open');
   });
 
-  closeButton.addEventListener('click', (event) => {
-    reset();
+  C2cSide.addEventListener('click', (event) => {
+    C2cDeskop.classList.remove('c2c-collapsed');
+    C2cSide.classList.remove('c2c-size-open');
   });
-
-  let back = document.getElementById('back');
-  back.addEventListener('click', (event) => {
-
-    if (backvalue == null){
-      reset();
-      return
-    }
-    // console.log(backvalue);
-
-    if (backvalue.includes('sc-')){
-      // back to firstchildren, hide secondchildren
-      secondchildrens.classList.add('d-none');
-      // hide elements of secondchildren
-      scdivs.forEach((cv, ci, listObj) => {
-        cv.classList.add('d-none');
-      });
-
-      // show firstchildren
-      firstchildrens.classList.remove('d-none');
-
-      updateSteps(false);
 
   C2cMobileHeader.addEventListener('click', (event) => {
     if(C2cMobile.classList.contains('c2c-mobile-open')) {
@@ -250,7 +92,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
   let phone = null;
 
   let c2cpopup = document.querySelector('.click-to-call--body .call-me-now');
-  c2cpopup.onclick = () => {
+  c2cpopup.onclick = (e) => {
+    e.preventDefault();
+    
     if(validationFields(".click-to-call--body")){
       const dataLead = {
         sou_id: 15,
@@ -295,9 +139,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     return true;
   }
 
-  function fillProgressBar(steps) {
-    document.querySelector(".bar").style.width = 100/steps + "%";
-  }
+
 
   // function getData(id) {
   //   const urlEndPoint = '/first-children';
