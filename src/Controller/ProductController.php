1e6f6d2d810899d8190f2c7b3038aea7f2ff731a
Jose del Río <josedelrio85@gmail.com>
2020-04-18 15:06:23 +0200

    improve template for products and added support to load the propper image in function of the landing source

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -15,6 +15,7 @@ class ProductController extends AbstractController {
 
   private $prodserv;
   private $repo;
+  private $request;
 
   public function __construct(i18nRepository $repo, ProductService $prodserv) {
     $this->prodserv = $prodserv;
@@ -22,9 +23,66 @@ class ProductController extends AbstractController {
   }
 
   public function index(Request $request, TranslatorInterface $translator) {
-
+    $this->request = $request;
     $productid = $request->attributes->get('_route');
-    // dump($this->repo->getMessages());
+    $hero = $this->repo->getKeyDataProducts($productid, 'hero');
+
+    $previous = $this->getPrevious();
+    if(is_null($previous)) {
+      $hero_image = $hero['images']['image1'];
+    } else {
+      $img = null;
+      switch($productid) {
+        case 'plena_plus':
+          switch($previous) {
+            case '/seguros-salud-familias':
+              $img = 'image1';
+            break;
+            case '/seguros-salud-individual':
+              $img = 'image2';
+            break;
+            case '/seguros-salud-embarazadas':
+              $img = 'image3';
+            break;
+          }
+        break;
+        case 'plena_vital':
+          switch($previous) {
+            case '/seguros-salud-familias':
+              $img = 'image1';
+            break;
+            case '/seguros-salud-individual':
+              $img = 'image2';
+            break;
+            case '/seguros-salud-embarazadas':
+              $img = 'image3';
+            break;
+          }
+        break;
+        case 'plena_extra':
+        case 'plena':
+        case 'go':
+          switch($previous) {
+            case '/seguros-salud-familias':
+              $img = 'image1';
+            break;
+            case '/seguros-salud-individual':
+              $img = 'image2';
+            break;
+          }
+        break;
+        break;
+        // TODO ADD REST OF PRODUCTS
+        case 'senior':
+          $img = 'image1';
+        break;
+        // case 'senior':
+        // break;
+      }
+      $hero_image = $hero['images'][$img];
+    }
+    // dump($hero_image);die();
+
     $price_bullets = $this->repo->getKeyDataProducts($productid,'price_with_details');
     $bullets = $this->repo->getKeyDataProducts($productid,'bullets');
     $des_with_dropdown = $this->repo->getKeyDataProducts($productid,'des_with_dropdown');
@@ -37,19 +95,32 @@ class ProductController extends AbstractController {
     // dump($des_with_dropdown);die();
     // dump($desc_with_bullets);die();
     // dump($dropdown);die();
+    // dump($faq);die();
     // dump($table);  die();
 
 
     return $this->render("pages/single-product.html.twig", [
       'product' => $productid,
       'translator' => $translator,
+      'hero_image' => $hero_image,
       'price_bullets' => $price_bullets['bullets'],
-      'bullets_list' => $bullets,
-      'des_with_dropdown_list' => $des_with_dropdown,
-      'desc_with_bullets_list' => $desc_with_bullets,
+      'bullets_extra' => $bullets,
+      'des_with_dropdown' => $des_with_dropdown,
+      'desc_with_bullets' => $desc_with_bullets,
       'dropdown_last' => $dropdown,
       'faq' => $faq,
       'table' => !empty($table) ? $table['table'] : null,
     ]);
+  }
+
+  private function getPrevious() {
+    $host = $this->request->getScheme(). "://" . $this->request->headers->get('host');
+    $referer = $this->request->headers->get('referer');
+
+    if(!empty($referer)) {
+      $previous = str_replace($host, '', $referer);
+      return $previous;
+    }
+    return null;
   }
 }
