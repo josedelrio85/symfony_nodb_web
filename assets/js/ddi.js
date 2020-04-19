73efa23bbd9d94c4bc34a87f403287e56ab39d19
Jose del Río <josedelrio85@gmail.com>
2020-04-19 14:34:13 +0200

    support to actions in slider button

--- assets/js/ddi.js
+++ assets/js/ddi.js
@@ -3,11 +3,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   let ddi = document.querySelector('.ddi-desktop');
   ddi.addEventListener('click', (e) => {
     $('#click-to-call-popup').modal('show');
-
-    let C2cDeskop = document.querySelector('.click-to-call-desktop');
-    let C2cSide = document.querySelector('.click-to-call-btn');
-    C2cDeskop.classList.add('c2c-collapsed');
-    C2cSide.classList.add('c2c-size-open');
+    closeC2C();
   });
 
   let ddimobile = document.querySelector('.ddi-mobile');
