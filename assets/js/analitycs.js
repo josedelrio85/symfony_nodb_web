8bb83157baff65d90241d3cb1f3e49f22760c228
Jose del Río <josedelrio85@gmail.com>
2020-04-27 17:57:19 +0200

    added support for view/scroll analytic events

--- assets/js/analitycs.js
+++ assets/js/analitycs.js
@@ -14,7 +14,14 @@ class analitycs {
             creative: null,         // Nombre de la creatividad
             position: null,         // Posición que ocupa dentro del slider o de la página
           }]
-        }
+        },
+        promoView: {
+          promotions: [{
+            name: null,             // Lugar donde se visualiza (home-slider | home-body | salud | salud-particulares | ......)
+            creative: null,         // Nombre de la creatividad
+            position: null,         // Posición que ocupa dentro del slider o de la página
+          }]
+        },
       }
     };
 
@@ -30,7 +37,20 @@ class analitycs {
     this.dataclick.ecommerce.promoClick.promotions[0].name = 'home-slider';
     this.dataclick.ecommerce.promoClick.promotions[0].creative = data.creative;
     this.dataclick.ecommerce.promoClick.promotions[0].position = data.position;
+    // delete this.dataclick.ecommerce.promoView;
+    // delete this.dataclick.noInteraction;
+    
     this.populateClick(this.dataclick);
+  }
+
+  sliderScroll = (data) => {
+    this.dataclick.eventLbl = 'home-slider';
+    this.dataclick.ecommerce.promoView.promotions[0].name = 'home-slider';
+    this.dataclick.ecommerce.promoView.promotions[0].creative = data.id;
+    this.dataclick.ecommerce.promoView.promotions[0].position = data.position;
+    // delete this.dataclick.ecommerce.promoClick;
+    
+    this.populateScroll(this.dataclick);
   }
 
   configurator = (data) => {
@@ -51,6 +71,30 @@ class analitycs {
     this.dataclick.ecommerce.promoClick.promotions[0].name = 'home-body';
     this.dataclick.ecommerce.promoClick.promotions[0].creative = pageName;
     this.dataclick.ecommerce.promoClick.promotions[0].position = data.position;
+    // delete this.dataclick.ecommerce.promoView;
+    // delete this.dataclick.noInteraction;
+
+    this.populateClick(this.dataclick);
+  }
+
+  configuratorScroll = (data) => {
+    let pageName = data.mc;
+    if (data.fc !== null && data.fc != undefined) {
+      pageName += "/" + data.fc;
+    }
+    if (data.sc !== null && data.sc != undefined) {
+      pageName += "/" + data.sc;
+    }
+
+    this.dataclick.eventLbl = 'home-body';
+    data.array.forEach(d => {
+      let promo = {
+        name : 'home-body',
+        creative : 'a',
+        position : d.position,
+      }
+      this.dataclick.ecommerce.promoClick.promotions.push(promo);
+    });
     this.populateClick(this.dataclick);
   }
 
@@ -59,8 +103,9 @@ class analitycs {
     this.dataclick.ecommerce.promoClick.promotions[0].name = data.name;
     this.dataclick.ecommerce.promoClick.promotions[0].creative = data.creative;
     this.dataclick.ecommerce.promoClick.promotions[0].position = data.position;
+    // delete this.dataclick.ecommerce.promoView;
+    // delete this.dataclick.noInteraction;
 
-    console.log(3);
     this.populateClick(this.dataclick);
   }
 
@@ -86,6 +131,27 @@ class analitycs {
             name: data.ecommerce.promoClick.promotions[0].name,
             creative: data.ecommerce.promoClick.promotions[0].creative,
             position: data.ecommerce.promoClick.promotions[0].position,
+          }]
+        }
+      }
+    });
+    console.log(window.dataLayer);
+  }
+
+  populateScroll = (data) => {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      event: 'eventoEC',
+      eventCat: 'ecommerce',
+      eventAct: 'promocionImpresion',
+      eventLbl: data.eventLbl,
+      noInteraction: true,
+      ecommerce: {
+        promoView: {
+          promotions: [{
+            name: data.ecommerce.promoView.promotions[0].name,
+            creative: data.ecommerce.promoView.promotions[0].creative,
+            position: data.ecommerce.promoView.promotions[0].position,
           }]
         }
       }
